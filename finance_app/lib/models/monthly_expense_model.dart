0c728f8a0f44794ddb4c6a758f1fe9f9adabaa4b
iambrianwalker <bwalker37837@gmail.com>
2026-03-11 11:05:13 -0400

    created toMap function for Monthly Expense model

--- finance_app/lib/models/monthly_expense_model.dart
+++ finance_app/lib/models/monthly_expense_model.dart
@@ -2,4 +2,18 @@ class MonthlyExpense{
   final int? id;
   final String expenseName;
   final String expenseAmount;
+
+  MonthlyExpense({
+    this.id,
+    required this.expenseName,
+    required this.expenseAmount
+  });
+
+  Map <String, dynamic> toMap() {
+    return{
+      'id' : id,
+      'expense_name' : expenseName,
+      'expense_amount' : expenseAmount,
+    };
+  }
 }
