ad799fdad8abbad6a11c75483692c51735d85543
iambrianwalker <bwalker37837@gmail.com>
2026-03-11 09:09:52 -0400

    Added class for job scenario model

---
+++ finance_app/lib/models/job_scenario_model.dart
@@ -0,0 +1,13 @@
+class JobScenario{
+  final int? id;
+  final String employerName;
+  final String payType;
+  final String wage;
+
+  JobScenario({
+    this.id,
+    required this.employerName,
+    required this.payType,
+    required this.wage
+  });
+}
